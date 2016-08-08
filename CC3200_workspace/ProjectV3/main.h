8d210e37a8885e227e34e204479af85471519476
taubrafi <taubrafi@gmail.com>
2016-08-08 14:13:45 -0400

    Initial

---
+++ CC3200_workspace/ProjectV3/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: Aug 4, 2016
+ *      Author: Rafi
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void shot_routine(void);
+
+
+
+#endif /* MAIN_H_ */
---
+++ CC3200_workspace/i2c_demo/Release/main.d
@@ -0,0 +1,43 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdio.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/linkage.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdarg.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/string.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdlib.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_types.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_ints.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_memmap.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_common_reg.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_map.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_patch.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/interrupt.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/prcm.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/utils.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/uart.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/example/common/uart_if.h
+main.obj: C:/ti/CC3200SDK_1.2.0/cc3200-sdk/example/common/i2c_if.h
+main.obj: ../pinmux.h
+
+../main.c: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdio.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/linkage.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdarg.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/string.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdlib.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_types.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_ints.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_memmap.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/inc/hw_common_reg.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_map.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/rom_patch.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/interrupt.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/prcm.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/utils.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/driverlib/uart.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/example/common/uart_if.h: 
+C:/ti/CC3200SDK_1.2.0/cc3200-sdk/example/common/i2c_if.h: 
+../pinmux.h: 
